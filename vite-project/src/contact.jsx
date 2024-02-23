56a98be11bfc76b0ecd8d7921e1155533bdcc1bb
nightmirror <nightmirror@nightmirordeair.mynetworksettings.com>
2024-02-22 21:28:27 -0500

    react-router and webpages added

---
+++ vite-project/src/contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const contact = () => {
+    return (
+        <div>
+            <h2>Hi, this is a testing message for contact.jsx</h2>
+        </div>
+    );
+};
+
+export default contact;
---
+++ vite-project/src/map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Map = () => {
+    return (
+        <div>
+            <h2>Hi, this is a testing message for map.jsx</h2>
+        </div>
+    );
+};
+
+export default Map;
