b87d624f8633d95a06259306122619d80d4c6fe9
nightmirror <nightmirror@nightmirordeair.mynetworksettings.com>
2024-03-03 16:47:00 -0500

    placeholder for map removed

--- vite-project/src/map.jsx
+++ vite-project/src/map.jsx
@@ -31,7 +31,6 @@ const Map = () => {
     return (
         <div>
             <img src= "/Images/Website Logo.png" alt=""/>
-            <h2>Hi, this is a testing message for map.jsx</h2>
             <div id="Research">
             <h2>Research</h2>
             <p>
