aa3ba75a78fec90666fe998d6b380f3809365583
Zarah Malik <zfm24@drexel.edu>
2024-04-08 20:04:47 -0400

    Inputted all data files into project folder

--- vite-project/src/reference.jsx
+++ vite-project/src/reference.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const reference = () => {
-    return (
-        <div>
-            <h2>Hi, this is a testing message for reference.jsx</h2>
-        </div>
+ const reference = () => {
+     return (
+         <div>
+             <h2>Hi, this is a testing message for reference.jsx</h2>
+         </div>
     );
 };
 
