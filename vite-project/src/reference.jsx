5ae3cd2a9329a0c0fde0f83311285dc1c8317939
Zarah Malik <zfm24@drexel.edu>
2024-04-28 15:34:36 -0400

    Manually added 8 more pinpoints around Philly to map

--- vite-project/src/reference.jsx
+++ vite-project/src/reference.jsx
@@ -3,7 +3,12 @@ import React from "react";
  const reference = () => {
      return (
          <div>
-             <h2>Hi, this is a testing message for reference.jsx</h2>
+             <h2>References for Crime Data: </h2>
+             <p>https://www.visitphilly.com/areas/philadelphia-neighborhoods/</p>
+             <p>https://drexel.edu/life-at-drexel/campus-safety</p>
+             <p>https://www.publicsafety.upenn.edu/safety-initiatives/pennguardian/</p>
+             <p>https://safety.temple.edu/tualert/tusafe</p>
+             <p>https://www.niche.com/places-to-live/n/rittenhouse-philadelphia-pa/#crime-safety</p>
          </div>
     );
 };
