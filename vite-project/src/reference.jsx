cbba4fe465e1bdeee8b2012743672ca8122da1a9
Zarah Malik <zfm24@drexel.edu>
2024-05-02 13:44:20 -0400

    Embedded links within Reference.jsx

--- vite-project/src/reference.jsx
+++ vite-project/src/reference.jsx
@@ -4,11 +4,33 @@ import React from "react";
      return (
          <div>
              <h2>References for Crime Data: </h2>
-             <p>https://www.visitphilly.com/areas/philadelphia-neighborhoods/</p>
-             <p>https://drexel.edu/life-at-drexel/campus-safety</p>
-             <p>https://www.publicsafety.upenn.edu/safety-initiatives/pennguardian/</p>
-             <p>https://safety.temple.edu/tualert/tusafe</p>
-             <p>https://www.niche.com/places-to-live/n/rittenhouse-philadelphia-pa/#crime-safety</p>
+             <div className="Referencelinks">
+                <br></br>
+                <a href="https://www.visitphilly.com/areas/philadelphia-neighborhoods">https://www.visitphilly.com/areas/philadelphia-neighborhoods</a>
+                <br></br>
+                <a href="https://drexel.edu/life-at-drexel/campus-safety">https://drexel.edu/life-at-drexel/campus-safety</a>
+                <br></br>
+                <a href="https://drexel.edu/life-at-drexel/campus-safety">https://drexel.edu/life-at-drexel/campus-safety</a>
+                <br></br>
+                <a href="https://www.publicsafety.upenn.edu/safety-initiatives/pennguardian/">https://www.publicsafety.upenn.edu/safety-initiatives/pennguardian/</a>
+                <br></br>
+                <a href="https://safety.temple.edu/tualert/tusafe">https://safety.temple.edu/tualert/tusafe</a>
+                <br></br>
+                <a href="https://www.niche.com/places-to-live/n/rittenhouse-philadelphia-pa/#crime-safety">https://www.niche.com/places-to-live/n/rittenhouse-philadelphia-pa/#crime-safety</a>
+                <br></br>
+                <a href="https://www.movoto.com/guide/philadelphia-pa/fairmount-park-philadelphia-neighborhood">https://www.movoto.com/guide/philadelphia-pa/fairmount-park-philadelphia-neighborhood</a>
+                <br></br>
+                <a href="https://www.niche.com/places-to-live/n/queen-village-philadelphia-pa/">https://www.niche.com/places-to-live/n/queen-village-philadelphia-pa/</a>
+                <br></br>
+                <a href="https://www.movoto.com/guide/philadelphia-pa/chinatown-philadelphia-neighborhood/">https://www.movoto.com/guide/philadelphia-pa/chinatown-philadelphia-neighborhood/</a>
+                <br></br>
+                <a href="https://www.niche.com/places-to-live/n/old-city-philadelphia-pa/">https://www.niche.com/places-to-live/n/old-city-philadelphia-pa/</a>
+                <br></br>
+                <a href="https://www.veryapt.com/guides/neighborhood/6-philadelphia-old-city/">https://www.veryapt.com/guides/neighborhood/6-philadelphia-old-city/</a>
+                <br></br>
+                <a href="https://www.veryapt.com/guides/neighborhood/6-philadelphia-old-city/">https://www.veryapt.com/guides/neighborhood/6-philadelphia-old-city/</a>
+                <br></br>
+             </div>
          </div>
     );
 };
